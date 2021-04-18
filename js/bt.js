13cde8052246b42da6837339f8c0be67d467e89c
msaqmar <aqmar12396@gmail.com>
2021-04-18 09:23:23 +0530

    First commit

---
+++ js/bt.js
@@ -0,0 +1,28 @@
+function bt1(){
+    window.location.href = "https://www.boat-lifestyle.com/products/airdopes-131-csk-edition";
+}
+function bt2(){
+    window.location.href = "https://www.boat-lifestyle.com/collections/top-wireless/products/rockerz-550?variant=31352999444578";
+}
+function bt3(){
+    window.location.href = "https://www.boat-lifestyle.com/collections/boat-best-sellers/products/boat-rockerz-255-pro-plus";
+}
+function bt4(){
+    window.location.href = "https://www.boat-lifestyle.com/collections/boat-best-sellers/products/stone-1500?variant=31997030006882";
+}
+
+
+
+// apple links
+function ap1(){
+    window.location.href = "https://www.apple.com/in/iphone-12-pro/";
+}
+function ap2(){
+    window.location.href = "https://www.apple.com/in/iphone-12/";
+}
+function ap3(){
+    window.location.href = "https://www.apple.com/in/macbook-air/";
+}
+function ap4(){
+    window.location.href = "https://www.apple.com/in/ipad-air/";
+}
